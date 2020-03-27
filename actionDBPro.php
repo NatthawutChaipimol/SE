7e973943dc50f104bec4e52cf04924f5ab75ca89
Peamsook <peamsook.ksw@gmail.com>
2020-03-28 01:36:43 +0700

    Update actionDBPro.php

--- actionDBPro.php
+++ actionDBPro.php
@@ -4,11 +4,8 @@ class ConnectDBPro
 {
     public function connect()
     {
-        $dbhost = "localhost";
-        $dbuser = "root";
         $dbpassword = "";
         $db = "db_se";
-        $conn = new mysqli($dbhost, $dbuser, $dbpassword, $db) or
             die("Connect failed %s\n" . $conn->erro);
         mysqli_set_charset($conn, "utf8");
         return $conn;
