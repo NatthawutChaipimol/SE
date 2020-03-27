2e0decaba6b90cca457ba9c0ebd9e0f52e5e9ab8
Peamsook <peamsook.ksw@gmail.com>
2020-03-28 00:02:14 +0700

    แก้ไขหน้าเพิ่ม

--- addPro.php
+++ addPro.php
@@ -36,6 +36,53 @@
         <link rel="stylesheet" href="bt.css" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
         <title>L'arbre Cafe</title>
+        <style>
+        #Time {
+            border: 1px solid #ccc;
+            color: #888;
+            margin: 0.5em;
+            vertical-align: middle;
+            outline: 0;
+            padding: 0.5em 1em;
+            border-radius: 4px;
+            width: calc(100% - 3em - 2px);
+            font-family: sans-serif;
+        }
+        #upload {
+            opacity: 0;
+        }
+
+        #upload-label {
+            position: absolute;
+            top: 50%;
+            left: 1rem;
+            transform: translateY(-50%);
+        }
+
+        .image-area {
+            border: 2px dashed rgba(255, 255, 255, 0.7);
+            padding: 1rem;
+            position: relative;
+        }
+
+        .image-area::before {
+            content: 'Uploaded image result';
+            color: #fff;
+            font-weight: bold;
+            text-transform: uppercase;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 0.8rem;
+            z-index: 1;
+        }
+
+        .image-area img {
+            z-index: 2;
+            position: relative;
+        }
+    </style>
     </head>
     <body>
         <div class="box">
@@ -53,8 +100,19 @@
                     <input type="text" id = "pAmount" name="pAmount" value="">
                 
                 <label class="LabelText" >สถานะของสินค้า</label>
-                <label><input type="radio" name="pStatus" value="0">ขาย</label>
-                <label><input type="radio" name="pStatus" value="1">ไม่ขาย</label><br>
+                <input type="radio" name="pStatus" value="0"><label>ขาย</label><br>
+                <input type="radio" name="pStatus" value="1"><label>ไม่ขาย</label> <br>
+
+                <label class="LabelText" >เลือกรูปของสินค้า</label>
+                <div class="input-group mb-3 px-2 py-2 rounded-pill bg-white shadow-sm">
+                    <input id="upload" type="file" onchange="readURL(this);" class="form-control border-0" name="seller_img">
+                    <label id="upload-label" for="upload" class="font-weight-light text-muted">Choose file</label>
+                    <div class="input-group-append">
+                        <label for="upload" class="btn btn-light m-0 rounded-pill px-4"> <i class="fa fa-cloud-upload mr-2 text-muted"></i><small class="text-uppercase font-weight-bold text-muted">Choose file</small></label>
+                    </div>
+                </div>
+
+                <div class="image-area mt-4"><img id="imageResult" name="img" src="" alt="" class="img-fluid rounded shadow-sm mx-auto d-block"></div>
 
                 <input type="submit" name="submit" value="บันทึก">
                 <input type="submit" name="submit" value="ยกเลิก">
@@ -63,7 +121,31 @@
         </form>
         </div>
     </div>
-
+    <script>
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imageResult')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $(function () {
+        $('#upload').on('change', function () {
+            readURL(input);
+        });
+    });
+    var input = document.getElementById( 'upload' );
+    var infoArea = document.getElementById( 'upload-label' );
+    input.addEventListener( 'change', showFileName );
+    function showFileName( event ) {
+        var input = event.srcElement;
+        var fileName = input.files[0].name;
+        infoArea.textContent = 'File name: ' + fileName;
+    }
+</script>
         <footer>
             
         </footer>
