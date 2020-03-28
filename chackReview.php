f7deb40d7c9a7a9f7df6bda4a83ce26d2318ca1f
Natthawut chipimol <natthawut_game2541@hotmail.com>
2020-03-28 19:13:01 +0700

    server

--- chackReview.php
+++ chackReview.php
@@ -1,13 +1,15 @@
 <?php
 require_once 'DBindex.php';
 error_reporting(E_ALL^E_NOTICE);
-
-$pId = $_POST['pId'];
+$j = $_POST['j'];
 $cId = 1;
 $date = date("Y-m-d h:i");
-$detail = $_POST['detail'];
-$score = $_POST['star'];
 $bId = $_POST['bId'];
-
-$con = new ConnectDBPro();
-$con->addReview($pId,$cId,$date,$detail,$score,$bId);
+for($i=0 ; $i<$j ; $i++){
+    echo $_POST['pId'.$i]."<br>";
+    echo $_POST['detail'.$i]."<br>";
+    echo $_POST['star'.$i]."<br>";
+    $con = new ConnectDBPro();
+    $con->addReview($_POST['pId'.$i],$cId,$date,$_POST['detail'.$i],$_POST['star'.$i],$bId);
+    header("Location:showStatusBill.php?bid=".$_POST['bId']);
+}
