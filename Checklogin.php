e0eb426187bf17afb9d7d79e24c3c9957969ed04
kittiya-m <60011212010@msu.ac.th>
2020-03-28 14:15:19 +0700

    jogin

---
+++ Checklogin.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+session_start();
+require_once './ConnectDatabase.php';
+$s = $_REQUEST["s"];
+if($s==1){
+    if($_POST['cUsername'] == 'admin' && $_POST['cPassword'] == '1234'){
+        $_SESSION['cUsername'] = $_POST['cUsername'];
+        $_SESSION['status'] = 1;
+    }else {
+        $conn = new ConnectDB();
+        $user = $conn->login($_POST['cUsername'], $_POST['cPassword']);
+        $_SESSION['cUsername'] = $_POST['cUsername'];
+    }
+    if($_SESSION['status'] == 1){
+        header("Location:index.php");
+    }else{
+        echo" Error";
+    }
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <link rel="stylesheet" href="bt.css" type="text/css">
+    <link rel="stylesheet" href="button.css" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <meta charset="UTF-8">
     <a class="navbar-brand text-light" href="index.php" style="color: #D8F4C6">
