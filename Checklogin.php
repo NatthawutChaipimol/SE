621d7a7af672c4c2f24d8fb8bec7b7bb0348f3e2
kittiya-m <60011212010@msu.ac.th>
2020-03-28 14:39:03 +0700

    Update Checklogin.php

--- Checklogin.php
+++ Checklogin.php
@@ -6,13 +6,13 @@ $s = $_REQUEST["s"];
 if($s==1){
     if($_POST['cUsername'] == 'admin' && $_POST['cPassword'] == '1234'){
         //$_SESSION['cUsername'] = $_POST['cUsername'];
-        $_SESSION['status'] = 1;
+        $_SESSION['status'] = '1';
     }else {
         $conn = new ConnectDB();
         $user = $conn->login($_POST['cUsername'], $_POST['cPassword']);
         $_SESSION['cUsername'] = $_POST['cUsername'];
     }
-    if($_SESSION['status'] == 1){
+    if($_SESSION['status'] == '1'){
         header("Location:index.php");
     }else{
         echo" Error";
