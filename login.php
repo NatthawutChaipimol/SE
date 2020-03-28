b1d71e39ec6c67847c8aa90834341f0049b1b715
kittiya-m <60011212010@msu.ac.th>
2020-03-28 11:49:27 +0700

    ds

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="bt.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <meta charset="UTF-8">
+</head>
+<body>
+<nav class="navbar navbar-light" style="background-color: #4C8577;">
+    <a class="navbar-brand text-light" href="index.php" style="color: #D8F4C6">
+        <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        L’ arbre Cafe
+    </a>
