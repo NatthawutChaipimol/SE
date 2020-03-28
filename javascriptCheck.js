daad69c6993100d02415df2e66b0a352aabcc984
kittiya-m <60011212010@msu.ac.th>
2020-03-28 16:47:21 +0700

    แก้

--- javascriptCheck.js
+++ javascriptCheck.js
@@ -2,6 +2,10 @@ function check(){
     var user = document.Check.user.value;
     var pass = document.Check.pass.value;
 
+    var user1 = document.Check.user.value;
+    var pass2 = document.Check.pass.value;
+
+
     if(user == ""){
         alert("Plaese input your Username!");
         return false;
@@ -9,4 +13,5 @@ function check(){
         alert("Plaese input your password!");
         return false;
     }
+
 }
