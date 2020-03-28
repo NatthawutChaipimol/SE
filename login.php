09434975f9eceb438bbcdea5088698596c7a022a
kittiya-m <60011212010@msu.ac.th>
2020-03-28 12:57:29 +0700

    Update login.php

--- login.php
+++ login.php
@@ -11,4 +11,24 @@
     <a class="navbar-brand text-light" href="index.php" style="color: #D8F4C6">
         <img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         L’ arbre Cafe
-    </a>
+	</a>
+	</nav>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width-device-width, initial-scale-1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="bt.css" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+		<title>L'arbre Cafe</title>
+		<div class="box">
+            <div id = "container">
+	
+	
